b2b90d23692baec8b68d299e22feb523eb541b1d
MixelMeza <mixolms@gmail.com>
2024-10-20 14:24:41 -0500

    v1

---
+++ src/main/java/pe/edu/upeu/pracway/Pracway/PracwayApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.pracway.Pracway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upeu/pracway/Pracway/PracwayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.pracway.Pracway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
