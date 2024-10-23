a81dd3ed2405c0ee9760b2ac8b7f446967178656
JuanEspiritu <sebastiandefray413@outlook.com>
2024-10-23 15:25:13 -0500

    v1

---
+++ src/main/java/pe/edu/upeu/pracway/Pracway/entity/Dia.java
@@ -0,0 +1,39 @@
+package pe.edu.upeu.pracway.Pracway.entity;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Data
+@Table(name = "Dia")
+public class Dia {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	private String nombre;
+	private boolean trabajado;
+	private char estado;
+	
+	@ManyToOne
+	@JoinColumn(name= "periodo_id",nullable= false)
+    private Periodo periodo;
+
+}
---
+++ src/main/java/pe/edu/upeu/pracway/Pracway/serviceImpl/DiaServiceImpl.java
@@ -0,0 +1,49 @@
+package pe.edu.upeu.pracway.Pracway.serviceImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pe.edu.upeu.pracway.Pracway.dao.DiaDao;
+import pe.edu.upeu.pracway.Pracway.entity.Dia;
+import pe.edu.upeu.pracway.Pracway.service.DiaService;
+
+@Service
+public class DiaServiceImpl implements DiaService{
+
+	@Autowired
+	private DiaDao diaDao;
+	
+	@Override
+	public Dia create(Dia d) {
+		// TODO Auto-generated method stub
+		return diaDao.create(d);
+	}
+
+	@Override
+	public Dia update(Dia d) {
+		// TODO Auto-generated method stub
+		return diaDao.update(d);
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		diaDao.delete(id);
+	}
+
+	@Override
+	public Optional<Dia> read(Long id) {
+		// TODO Auto-generated method stub
+		return diaDao.read(id);
+	}
+
+	@Override
+	public List<Dia> readAll() {
+		// TODO Auto-generated method stub
+		return diaDao.readAll();
+	}
+
+}
